fixed formula

onebatchman/model.py
@@ -1,7 +1,7 @@
 import numpy as np
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Activation, BatchNormalization, Input, Softmax, ReLU
-from tensorflow.keras.activations import relu
+from tensorflow.keras.activations import relu, softmax
 from tensorflow.keras.optimizers import Adam
 from .replybuffer import ReplayBuffer
 
@@ -15,7 +15,7 @@ def build_dqn(lr, input_dims, dense_dims, n_actions):
     # layers.append(BatchNormalization())
   
   layers.append(Dense(n_actions))
-  layers.append(Activation(relu))
+  layers.append(Activation(softmax))
 
   model = Sequential(layers)
   model.compile(optimizer=Adam(lr=lr), loss='mse')
@@ -48,14 +48,20 @@ class Brain(object):
       pred = self.network.predict(state, verbose=0)
       # TODO add activation of only those that are proper moves
       next = self.network.predict(new_state, verbose=0)
+      # print(next)
+      next_action = next.argmax(axis=1)
+      # print(next_action)
+      # print()
 
       batch_index = np.arange(self.batch_size, dtype=np.int8)
-      
+
       # print(f"state: {state}")
       # print(f"action: {action}")
       # print(f"reward: {reward}")
-
-      pred[batch_index, action] = reward + self.gamma * next[batch_index, action] * (1 - done)
+      pred[batch_index, action] = reward * 1e-2 + self.gamma * next[batch_index, next_action] * (1 - done)
+      # print("target")
+      # print(pred)
+      # print()
 
       self.history.append(self.network.fit(state, pred, verbose=0).history['loss'])
 

onebatchman/player.py
@@ -22,6 +22,7 @@ class OneBatchMan(Player):
     self.mvs = 0
     self.random_mvs = 0
     self.model = Brain(alpha=2e-2, gamma=0.92, batch_size=32, input_dims=(3*24 + 4), mem_size=1_000, dnss=[48, 48], n_actions=24)
+    self.last_pred = None
 
   def make_move(self, game_state: dict, was_previous_move_wrong: bool) -> Card:
     self.mvs += 1
@@ -37,6 +38,8 @@ class OneBatchMan(Player):
     self.previous_action = self.action
     self.cur_state = self.create_state_vector(game_state)
     pred = self.model.predict(self.cur_state)[0]
+    self.last_pred = pred
+    # print(pred)
     self.action = self.pick_move(game_state, pred, self.model.epsilon)
 
     # for this to work, all states, actions and rewards have to be nulled, so it wont go into hell by accident
@@ -48,7 +51,7 @@ class OneBatchMan(Player):
 
   def pick_move(self, game_state, pred, epsilon):
     if random.random() < epsilon:
-      return card_id(random.choice(game_state['hand']))
+      return card_id(random.choice(self.legal_moves(game_state)))
 
     possible_actions = self.get_legal_idx(game_state)
     actions_matrix: np.ndarray = pred.take(possible_actions) +1e-8